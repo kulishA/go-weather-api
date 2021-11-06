1fdb4790fd170bd0a8ee45dae90a7663bd1c1c90
Anton Kulishenko <anton.kulishenko95@gmail.com>
2021-11-06 02:16:43 +0200

    added init postgres repository

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,39 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	weatherapi "github.com/kulishA/go-weather-api"
 	"github.com/kulishA/go-weather-api/pkg/handler"
+	"github.com/kulishA/go-weather-api/pkg/repository"
+	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"os"
 )
 
 func main() {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("errror initializing configs: %s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalf("errror loading env variables: %s", err.Error())
+	}
+
+	_, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("POSTGRES_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+
+	if err != nil {
+		logrus.Fatalf("failded to initialize DB: %s", err.Error())
+	}
 
 	handlers := handler.NewHandler()
 
@@ -14,4 +41,11 @@ func main() {
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		logrus.Fatalf("error occurred while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
